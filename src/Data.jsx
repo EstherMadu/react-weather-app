35f4b8e37a0ae71a2058ae3a72ed42dc1b3cb9ea
esthermadu <prityexsta@gmail.com>
2023-04-11 15:31:56 +0100

    added states

--- src/Data.jsx
+++ src/Data.jsx
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import Weather from "./Weather";
 
 const apiKey = "c1d7db6fd0964748bf6121505231104";
-const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Nigeria`;
 
 const FetchData = () => {
   const [weather, setWeather] = useState({});
+  const [state, setState] = useState("Abuja");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${state}`;
         const resp = await fetch(url);
         const data = await resp.json();
         console.log(data);
@@ -20,10 +21,55 @@ const FetchData = () => {
     };
 
     fetchData();
-  }, []);
-  console.log(weather);
+  }, [state]);
+
+  const handleChange = (event) => {
+    setState(event.target.value);
+  };
   return (
     <section>
+      <div>
+        <label htmlFor="state">Select State: </label>
+        <select name="state" id="state" value={state} onChange={handleChange}>
+          <option value="Abia">Abia</option>
+          <option value="Abuja">Abuja</option>
+          <option value="Adamawa">Adamawa</option>
+          <option value="Akwa Ibom">Akwa Ibom</option>
+          <option value="Anambra">Anambra</option>
+          <option value="Bauchi">Bauchi</option>
+          <option value="Bayelsa">Bayelsa</option>
+          <option value="Benue">Benue</option>
+          <option value="Borno">Borno</option>
+          <option value="Cross River">Cross River</option>
+          <option value="Delta">Delta</option>
+          <option value="Ebonyi">Ebonyi</option>
+          <option value="Edo">Edo</option>
+          <option value="Ekiti">Ekiti</option>
+          <option value="Enugu">Enugu</option>
+          <option value="Gombe">Gombe</option>
+          <option value="Imo">Imo</option>
+          <option value="Jigawa">Jigawa</option>
+          <option value="Kaduna">Kaduna</option>
+          <option value="Kano">Kano</option>
+          <option value="Katsina">Katsina</option>
+          <option value="Kebbi">Kebbi</option>
+          <option value="Kogi">Kogi</option>
+          <option value="Kwara">Kwara</option>
+          <option value="Lagos">Lagos</option>
+          <option value="Nasarawa">Nasarawa</option>
+          <option value="Niger">Niger</option>
+          <option value="Ogun">Ogun</option>
+          <option value="Ondo">Ondo</option>
+          <option value="Osun">Osun</option>
+          <option value="Oyo">Oyo</option>
+          <option value="Plateau">Plateau</option>
+          <option value="Rivers">Rivers</option>
+          <option value="Sokoto">Sokoto</option>
+          <option value="Taraba">Taraba</option>
+          <option value="Yobe">Yobe</option>
+          <option value="Zamfara">Zamfara</option>
+        </select>
+      </div>
       {Object.keys(weather).length > 0 && (
         <Weather
           name={weather.location.name}
